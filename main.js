4a762a3344164e2ae1163afda06b465e2de93f1f
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-07-18 17:06:18 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+{
+  let isDevMode = true;
+  const pElement = document.querySelector("p");
+  const buttonElement = document.querySelector("button");
+  buttonElement.addEventListener("click", () => {
+    if (!confirm("Are you sure?")) {
+      return;
+    }
+
+    if (isDevMode) {
+      pElement.textContent = "Dev Mode is Off";
+    } else {
+      pElement.textContent = "Dev Mode is On";
+    }
+    isDevMode = !isDevMode;
+  });
+}
